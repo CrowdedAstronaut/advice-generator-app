350d1ccbb9026858278f17dacff703dc5ffa8b6e
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-07 09:33:22 -0600

    created variables for title and text

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
+const title = document.getElementById("title");
+const text = document.getElementById("text");
+
 const getAdvice = async () => {
   const result = await fetch(
     "https://api.adviceslip.com/advice"
   );
   const data = await result.json();
-
   console.log(data);
 
-  document.getElementById(
-    "title"
-  ).innerHTML = `Advice #${data.slip.id}`;
-  document.getElementById(
-    "text"
-  ).innerHTML = `"${data.slip.advice}"`;
+  title.innerHTML = `Advice #${data.slip.id}`;
+  text.innerHTML = `"${data.slip.advice}"`;
 };
 
 window.addEventListener("load", () => {

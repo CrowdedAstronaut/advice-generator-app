25c46da5fc4052d68bdbc1a556c0bc8a51bf4dce
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-07 09:42:57 -0600

    Changed event listener to DOMContentLoaded

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ const title = document.getElementById("title");
 const text = document.getElementById("text");
 
 const getAdvice = async () => {
-  const result = await fetch(
+  let result = await fetch(
     "https://api.adviceslip.com/advice"
   );
-  const data = await result.json();
+  let data = await result.json();
   console.log(data);
 
   title.innerHTML = `Advice #${data.slip.id}`;
   text.innerHTML = `"${data.slip.advice}"`;
 };
 
-window.addEventListener("load", () => {
+window.addEventListener("DOMContentLoaded", () => {
   getAdvice();
 });
